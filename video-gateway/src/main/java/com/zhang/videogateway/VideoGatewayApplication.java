b786c4d7b7b2d07255dfc346c5046e29b1f44894
a510334890 <510334890@qq.com>
2021-03-19 17:07:47 +0800

    增加网关gateway

---
+++ video-gateway/src/main/java/com/zhang/videogateway/VideoGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.zhang.videogateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoGatewayApplication.class, args);
+    }
+
+}
