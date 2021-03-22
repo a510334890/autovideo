7489e1d7ed0d683f42717c63c235d01661342590
a510334890 <510334890@qq.com>
2021-03-22 17:56:23 +0800

    主页修改增加用户信息

--- App/src/main/java/com/zhang/app/controller/UserController.java
+++ App/src/main/java/com/zhang/app/controller/UserController.java
@@ -4,10 +4,12 @@ package com.zhang.app.controller;
 import com.zhang.app.service.UserFeignapi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -15,17 +17,21 @@ import java.util.List;
 public class UserController {
     @Autowired
     private UserFeignapi feignapi;
-    @ResponseBody
-    @RequestMapping("/getuser")
-    public List getuser(){
-        return feignapi.getuser();
-    }
+
     @RequestMapping("/getlogin")
     public String getlogin(){
         return "login";
     }
     @RequestMapping("/getindex")
-    public String getindex(){
+    public String getindex(HttpSession session,String username,String password){
+        List getuser = feignapi.getuser();
+        session.setAttribute("userList",getuser);
         return "index";
+    }
+    @RequestMapping("/gettable")
+    public String gettable(Model model){
+        List getuser = feignapi.getuser();
+        model.addAttribute("userlist",getuser);
+        return "usertables";
     }
 }
