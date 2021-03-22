12542e54e006f4414cf436b76b611a0066af91ea
a510334890 <510334890@qq.com>
2021-03-22 10:15:20 +0800

    修改app模块的feign客户端

--- App/src/main/java/com/zhang/app/service/UserFeignapi.java
+++ App/src/main/java/com/zhang/app/service/UserFeignapi.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@FeignClient(name = "userserver,userserver02")
+@FeignClient(name = "userserver")
 public interface UserFeignapi {
     @RequestMapping("user/getuser")
     List getuser();
